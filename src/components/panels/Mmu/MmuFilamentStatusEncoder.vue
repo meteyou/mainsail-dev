206fb9f53cd34e3c0d12c498e56e0456c60d6e93
moggieuk <32878385+moggieuk@users.noreply.github.com>
2025-11-06 13:07:06 +0700

    Also fixed the encoder styles and font color

--- src/components/panels/Mmu/MmuFilamentStatusEncoder.vue
+++ src/components/panels/Mmu/MmuFilamentStatusEncoder.vue
@@ -1,9 +1,9 @@
 <template>
     <g>
-        <circle cx="258" cy="140" r="8" stroke-width="1" :class="encoderClass" />
+        <circle cx="258" cy="140" r="8" stroke-width="1" :class="circleClass" />
         <path d="M257 135 L261 140 L257 145" stroke-width="2" fill="none" />
-        <text x="278" y="145">{{ $t('Panels.MmuPanel.Encoder') }}</text>
-        <text x="345" y="145" font-size="11px">{{ encoderPosText }}</text>
+        <text x="278" y="145" :class="textClass">{{ $t('Panels.MmuPanel.Encoder') }}</text>
+        <text x="345" y="145" font-size="11px" :class="textClass">{{ encoderPosText }}</text>
         <transition name="fade">
             <text v-if="homedToEncoder" x="219.5" y="145" font-weight="bold">H</text>
         </transition>
@@ -22,13 +22,6 @@ import MmuMixin, {
 
 @Component
 export default class MmuFilamentStatusEncoder extends Mixins(BaseMixin, MmuMixin) {
-    get encoderClass() {
-        // TODO: Need to separate encoder runout disable from general availability (like other sensors)
-        if (this.mmuFilamentPos === FILAMENT_POS_UNLOADED) return 'sensor-disabled'
-
-        return this.encoderPos ? 'sensor-triggered' : 'sensor-open'
-    }
-
     get encoderPosText() {
         return this.encoderPos < 10000 ? `${this.encoderPos} mm` : `${this.encoderPos}`
     }
@@ -48,5 +41,43 @@ export default class MmuFilamentStatusEncoder extends Mixins(BaseMixin, MmuMixin
     get encoderPos() {
         return Math.round(this.mmuEncoder?.encoder_pos ?? 0)
     }
+
+    get circleClass() {
+        return this.mmuEncoder.enabled ? 'sensor-normal' : 'sensor-disabled'
+    }
+
+    get textClass() {
+        return {
+            'text-disabled': !this.mmuEncoder.enabled,
+        }
+    }
 }
 </script>
+
+<style scoped>
+text {
+    fill: currentColor;
+}
+
+.text-disabled {
+    opacity: 0.5;
+}
+
+.sensor-disabled {
+    stroke: var(--disabled-stroke);
+    stroke-dasharray: 2, 1;
+    fill: var(--zone-background-dark-theme);
+}
+
+html.theme--light .sensor-disabled {
+    fill: var(--zone-background-light-theme);
+}
+
+.sensor-normal {
+    fill: var(--zone-background-dark-theme);
+}
+
+html.theme--light .sensor-normal {
+    fill: var(--zone-background-light-theme);
+}
+</style>
